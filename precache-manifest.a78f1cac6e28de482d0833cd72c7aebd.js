a6af128277f57c1373e7b4de30f1acc3b233b679
ZalotBabyZ <babyzalot@gmail.com>
2021-01-09 18:13:50 +0700

    Updates

---
+++ precache-manifest.a78f1cac6e28de482d0833cd72c7aebd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f3e696bba4f9a88571fc6aca715d21",
+    "url": "/MiniProject_TossGame/index.html"
+  },
+  {
+    "revision": "0332c9b1aabcc42aa8ce",
+    "url": "/MiniProject_TossGame/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "be879b3867cc56e34c60",
+    "url": "/MiniProject_TossGame/static/js/2.a4d74fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MiniProject_TossGame/static/js/2.a4d74fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0332c9b1aabcc42aa8ce",
+    "url": "/MiniProject_TossGame/static/js/main.83feac5d.chunk.js"
+  },
+  {
+    "revision": "044395a4be4735ef57c7",
+    "url": "/MiniProject_TossGame/static/js/runtime-main.0919244a.js"
+  },
+  {
+    "revision": "43522caf8e30a705a210bed900bc08a8",
+    "url": "/MiniProject_TossGame/static/media/DICE1.43522caf.png"
+  },
+  {
+    "revision": "a5b05db99e2b715015fc3583c6f1f045",
+    "url": "/MiniProject_TossGame/static/media/DICE2.a5b05db9.png"
+  },
+  {
+    "revision": "23f1f26ccd72d5df33b9248136c62393",
+    "url": "/MiniProject_TossGame/static/media/DICE3.23f1f26c.png"
+  },
+  {
+    "revision": "0b33515cf19fd0d7ff5ac433464b5682",
+    "url": "/MiniProject_TossGame/static/media/DICE4.0b33515c.png"
+  },
+  {
+    "revision": "23d7427a4ccadb689e3023f58c597fd1",
+    "url": "/MiniProject_TossGame/static/media/DICE5.23d7427a.png"
+  },
+  {
+    "revision": "5419ac099e71add614ee56ad89c1e4cf",
+    "url": "/MiniProject_TossGame/static/media/DICE6.5419ac09.png"
+  }
+]);
